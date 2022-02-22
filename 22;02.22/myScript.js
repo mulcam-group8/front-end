3c82004f9a8235277077efbd5799ad0e45f8358c
yuuulya <yla101@naver.com>
2022-02-23 01:01:00 +0900

    22.02.22

---
+++ 22;02.22/myScript.js
@@ -0,0 +1,75 @@
+function productClick1(){
+    
+}
+const p1 = document.querySelector("#p1");
+const p2 = document.querySelector("#p2");
+const p3 = document.querySelector("#p3");
+const p4 = document.querySelector("#p4");
+const p5 = document.querySelector("#p5");
+const p6 = document.querySelector("#p6");
+const p7 = document.querySelector("#p7");
+const p8 = document.querySelector("#p8");
+const p9 = document.querySelector("#p9");
+const p10 = document.querySelector("#p10");
+const p11 = document.querySelector("#p11");
+const p12 = document.querySelector("#p12");
+const box = document.querySelector(".box");
+
+function HandleMouseEnter(){
+   box.style.color="rgb(208, 181, 164)";
+}
+box.addEventListener("mouseenter", HandleMouseEnter);
+function HandleMouseLeave(){
+   box.style.color="black";
+}
+box.addEventListener("mouseleave", HandleMouseLeave);
+   function productClick1(){
+      alert("상품페이지로 이동합니다.");//새창 열기로 바꾸기
+   }
+   p1.addEventListener("click", productClick1);
+
+   function productClick2(){
+      alert("상품페이지로 이동합니다.");//새창 열기로 바꾸기
+   }
+   p2.addEventListener("click", productClick2);
+
+   function productClick3(){
+      alert("상품페이지로 이동합니다.");//새창 열기로 바꾸기
+   }
+   p3.addEventListener("click", productClick3);
+   function productClick4(){
+      alert("상품페이지로 이동합니다.");//새창 열기로 바꾸기
+   }
+   p4.addEventListener("click", productClick4);
+   function productClick5(){
+      alert("상품페이지로 이동합니다.");//새창 열기로 바꾸기
+   }
+   p5.addEventListener("click", productClick5);
+   function productClick6(){
+      alert("상품페이지로 이동합니다.");//새창 열기로 바꾸기
+   }
+   p6.addEventListener("click", productClick6);
+   function productClick7(){
+      alert("상품페이지로 이동합니다.");//새창 열기로 바꾸기
+   }
+   p7.addEventListener("click", productClick7);
+   function productClick8(){
+      alert("상품페이지로 이동합니다.");//새창 열기로 바꾸기
+   }
+   p8.addEventListener("click", productClick8);
+   function productClick9(){
+      alert("상품페이지로 이동합니다.");//새창 열기로 바꾸기
+   }
+   p9.addEventListener("click", productClick9);
+   function productClick10(){
+      alert("상품페이지로 이동합니다.");//새창 열기로 바꾸기
+   }
+   p10.addEventListener("click", productClick10);
+   function productClick11(){
+      alert("상품페이지로 이동합니다.");//새창 열기로 바꾸기
+   }
+   p11.addEventListener("click", productClick11);
+   function productClick12(){
+      alert("상품페이지로 이동합니다.");//새창 열기로 바꾸기
+   }
+   p12.addEventListener("click", productClick12);
---
+++ 22;02.22/script.js
@@ -0,0 +1,15 @@
+			function imgButtonClick1()
+			{
+				document.querySelector('.imgcontainer').style.transform = 'translate(0vw)';
+			}
+			
+			function imgButtonClick2()
+			{
+				document.querySelector('.imgcontainer').style.transform = 'translate(-100vw)';
+			}
+			
+			function imgButtonClick3()
+			{
+				document.querySelector('.imgcontainer').style.transform = 'translate(-200vw)';
+			}
+			
