8bc6ee550cdaad8e06eb46c31f0ee77f8dc107ba
yuuulya <yla101@naver.com>
2022-02-23 17:32:36 +0900

    front-end_final

---
+++ front-end_final/WebContent/script.js
@@ -0,0 +1,79 @@
+			function imgButtonClick1()
+			{
+				document.querySelector('.imgcontainer').style.transform = 'translate(0vw)';
+			}
+			
+			function imgButtonClick2()
+			{
+				document.querySelector('.imgcontainer').style.transform = 'translate(-100vw)';
+			}
+			
+			function imgButtonClick3()
+			{
+				document.querySelector('.imgcontainer').style.transform = 'translate(-200vw)';
+			}
+			
+const p1 = document.querySelector("#p1");
+const p2 = document.querySelector("#p2");
+const p3 = document.querySelector("#p3");
+const p4 = document.querySelector("#p4");
+const p5 = document.querySelector("#p5");
+const p6 = document.querySelector("#p6");
+const p7 = document.querySelector("#p7");
+const p8 = document.querySelector("#p8");
+const p9 = document.querySelector("#p9");
+const p10 = document.querySelector("#p10");
+const p11 = document.querySelector("#p11");
+const p12 = document.querySelector("#p12");
+
+ function productClick1(){
+     location.href="details.html";
+   }
+   p1.addEventListener("click", productClick1);
+
+
+  function productClick2(){
+      location.href="details.html";
+   }
+   p2.addEventListener("click", productClick2);
+
+   function productClick3(){
+      location.href="details.html";
+   }
+   p3.addEventListener("click", productClick3);
+   function productClick4(){
+      location.href="details.html";
+   }
+   p4.addEventListener("click", productClick4);
+   function productClick5(){
+      location.href="details.html";
+   }
+   p5.addEventListener("click", productClick5);
+   function productClick6(){
+      location.href="details.html";
+   }
+   p6.addEventListener("click", productClick6);
+   function productClick7(){
+      location.href="details.html";
+   }
+   p7.addEventListener("click", productClick7);
+   function productClick8(){
+      location.href="details.html";
+   }
+   p8.addEventListener("click", productClick8);
+   function productClick9(){
+      location.href="details.html";
+   }
+   p9.addEventListener("click", productClick9);
+   function productClick10(){
+      location.href="details.html";
+   }
+   p10.addEventListener("click", productClick10);
+   function productClick11(){
+      location.href="details.html";
+   }
+   p11.addEventListener("click", productClick11);
+   function productClick12(){
+      location.href="details.html";
+   }
+   p12.addEventListener("click", productClick12);
